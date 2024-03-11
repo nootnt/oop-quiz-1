803bedf08a14fd819457b58bce5583ef43e3f6b0
nootnt <DefenestrationUsurper@proton.me>
2024-03-11 11:14:45 +0400

    assignment 1

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+package quizes.q1.nikolozi_lagvilava_1;
+/**
+ * Corrected :)
+ */
+public class Assignment01{
+    public static void main(String[] args){
+
+        int A = 17;
+        int B = 18;
+
+        System.out.println("The sum of the numbers is " + (A + B));
+    }
+}
